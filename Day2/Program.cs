88c1d2f0a91aa35730d4365e5b7f37b3a1fefdb8
mrkalle <carl.a.j.lundin@gmail.com>
2016-12-02 08:12:06 +0100

    Start på Day2

---
+++ Day2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // Läs in input
+        // Loopa över input, ha en senaste-nr-variabel (start är 5)
+        
+    }
+}
